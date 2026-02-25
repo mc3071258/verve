3915cb4d2ca043fb1374393f1753a07c8c71012b
Magnus <255735697+mc3071258@users.noreply.github.com>
2026-02-25 15:50:14 +0000

    Set up static and media directories in project settings

--- partygames/settings.py
+++ partygames/settings.py
@@ -121,3 +121,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
