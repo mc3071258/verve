babbc6df6618a35da905283a6ebaa9fcec586ca6
Magnus <255735697+mc3071258@users.noreply.github.com>
2026-02-25 03:43:35 +0000

    Initial Django project setup with core app

---
+++ partygames/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for partygames project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'partygames.settings')
+
+application = get_wsgi_application()
