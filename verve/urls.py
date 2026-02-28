201b16cafe1fd015b8ce5d1233fb814e74e56c3e
Magnus C <255735697+mc3071258@users.noreply.github.com>
2026-02-28 01:27:20 +0000

    Rename django project package to verve

--- verve/urls.py
+++ verve/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for partygames project.
+URL configuration for verve project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
