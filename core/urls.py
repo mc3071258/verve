fbfb75c3b78e2273b7400241b5b2fe1a6de78b59
Magnus <255735697+mc3071258@users.noreply.github.com>
2026-03-09 00:51:12 +0000

    Set up URL routes

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,21 @@ from django.http import HttpResponse
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("home/", views.home), # Add redirect later
+
+    path("game/<slug:slug>/", views.game, name="game"),
+    path("game/<slug:slug>/play/", views.game_play, name="game_play"),
+    path("game/<slug:slug>/view/", views.game_prompts, name="game_prompts"),
+
+    path("create-prompt/", views.create_prompt, name="create_prompt"),
+
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
+    path("logout/", views.logout, name="logout"),
+
+    path("profiles/", views.my_profile, name="my_profile"),
+    path("profiles/edit/", views.my_profile_edit, name="edit_profile"),
+    path("profiles/<str:username>/", views.profile, name="profile"),
 ]
 
-def home(request):
-    return HttpResponse("Home page works")  
+
--- core/views.py
+++ core/views.py
@@ -4,5 +4,37 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.http import HttpResponse
+
 def home(request):
-    return render(request, "core/home.html")
+    return HttpResponse("Home page works")
+
+def game(request, slug):
+    return HttpResponse(f"Game page works: {slug}")
+
+def game_play(request, slug):
+    return HttpResponse(f"Game play works: {slug}")
+
+def game_prompts(request, slug):
+    return HttpResponse(f"Game prompts works: {slug}")
+
+def create_prompt(request):
+    return HttpResponse("Create prompt page works")
+
+def login(request):
+    return HttpResponse("Login page works")
+
+def register(request):
+    return HttpResponse("Register page works")
+
+def logout(request):
+    return HttpResponse("Logout page works")
+
+def my_profile(request):
+    return HttpResponse("My profile page works")
+
+def my_profile_edit(request):
+    return HttpResponse("Edit profile page works")
+
+def profile(request, username):
+    return HttpResponse(f"Public profile works: {username}")
