3406a6177e548b77b16c386f09d71c4529016f1a
Eva De Luca <3012530d@student.gla.ac.uk>
2026-03-11 12:41:17 +0000

    add underline

---
+++ templates/basejs.js
@@ -0,0 +1,9 @@
+const navLinks = document.querySelectorAll('.NavLoggedIn .nav-link');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    navLinks.forEach(l => l.classList.remove('active'));
+    link.classList.add('active');
+  });
+});
