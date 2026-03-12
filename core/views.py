b0fc9665305d38772842f0cdc391bb59d1eef309
Red Parry <3071034p@student.gla.ac.uk>
2026-03-12 01:49:11 +0000

    basic edit prompts function

--- core/views.py
+++ core/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
-from core.forms import UserForm, UserProfileForm
-from core.models import Prompt
+from core.forms import UserForm, UserProfileForm, PromptForm
+from core.models import Prompt, Vote
 
 def home(request):
     return render(request, "home.html")
@@ -63,7 +63,15 @@ def game_prompts(request, slug):
 # Prompts
 @login_required
 def create_prompt(request):
-    return render(request, "prompts/create.html")
+    form = PromptForm()
+    if request.method == "POST":
+        form = PromptForm(request.POST)
+        new_prompt = form.save(commit=False)
+        new_prompt.creator = request.user
+        new_prompt.save()
+        return redirect("my_prompts")
+
+    return render(request, "prompts/create.html", {"form":form})
 
 
 # Profiles
@@ -80,13 +88,33 @@ def my_prompts(request):
     context_dict = {"edit_mode": False}
     current_user = request.user
     user_prompts = Prompt.objects.filter(creator=current_user)
-    context_dict["prompts"] = user_prompts
 
+    votes_list = []
+    for cur_prompt in user_prompts:
+        votes = Vote.objects.filter(prompt=cur_prompt)
+        votes_list.append(votes.__len__())
+    context_dict["prompts"] = zip(user_prompts, votes_list)
+    
     return render(request, "profiles/my_prompts.html", context_dict)
 
 @login_required
 def edit_prompts(request):
-    context_dict = {"edit_mode": True}
+    if request.method == "POST":
+        prompt_text = request.POST.get("prompt_inst")
+        instance = Prompt.objects.get(text=prompt_text)
+        instance.text = request.POST.get("text")
+        instance.save()
+
+    context_dict = {"edit_mode":True}
+    current_user = request.user
+    user_prompts = Prompt.objects.filter(creator=current_user)
+
+    votes_list = []
+    for cur_prompt in user_prompts:
+        votes = Vote.objects.filter(prompt=cur_prompt)
+        votes_list.append(votes.__len__())
+    context_dict["prompts"] = zip(user_prompts, votes_list)
+
     return render(request, "profiles/my_prompts.html", context_dict)
 
 def profile(request, username):
