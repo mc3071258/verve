ef4aa3bf3d6fb990ecbe3050c4fee2a4d0fbd237
Magnus <255735697+mc3071258@users.noreply.github.com>
2026-03-23 19:09:12 +0000

    Clean settings

--- verve/settings.py
+++ verve/settings.py
@@ -124,8 +124,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
 LOGIN_URL = "login"
