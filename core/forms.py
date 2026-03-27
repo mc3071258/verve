c6bf46802dafa626021d4da3796be39d4bcc66dc
Magnus <255735697+mc3071258@users.noreply.github.com>
2026-03-27 11:34:25 +0000

    Add Sting bug safeguard and align form with database

--- core/forms.py
+++ core/forms.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 
 class BasePromptForm(forms.ModelForm):
-    text = forms.CharField(required=True, max_length=250, help_text="Please enter the prompt") 
+    text = forms.CharField(required=True, max_length=100, help_text="Please enter the prompt") 
 
     class Meta:
         model = Prompt
@@ -23,8 +23,8 @@ class TruthOrDareForm(BasePromptForm):
         fields = ["text", "category"]
 
 class WouldYouRatherForm(forms.ModelForm):
-    optionA = forms.CharField(required=True, max_length=250, help_text="Please enter option 1")
-    optionB = forms.CharField(required=True, max_length=250, help_text="Please enter option 2")
+    optionA = forms.CharField(required=True, max_length=50, help_text="Please enter option 1")
+    optionB = forms.CharField(required=True, max_length=50, help_text="Please enter option 2")
 
     class Meta:
         model = Prompt
